9243225d819f99453f29518f7e7fc40ccfa8d9b3
Humayra-Amin <humaema4576@gmail.com>
2024-05-10 00:21:39 +0600

    client setup done with firebase

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAuVXBJmhqFbKKlueMyWV5OK-ruJcVnaEE",
+  authDomain: "online-group-study-d5764.firebaseapp.com",
+  projectId: "online-group-study-d5764",
+  storageBucket: "online-group-study-d5764.appspot.com",
+  messagingSenderId: "148913071693",
+  appId: "1:148913071693:web:d4e5c8e6df5fa63c629e04"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
