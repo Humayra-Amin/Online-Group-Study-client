64a14d2969322beeca9bc8db344016f7c1154c2c
Humayra-Amin <humaema4576@gmail.com>
2024-05-15 01:01:50 +0600

    filter added

--- src/components/Assignments/Assignments.jsx
+++ src/components/Assignments/Assignments.jsx
@@ -1,12 +1,39 @@
 import { useLoaderData } from "react-router-dom";
 import AllAssignments from "../AllAssignments/AllAssignments";
+import { useState } from "react";
 
 const Assignments = () => {
     const allAssignment = useLoaderData();
+
+    const [filterBy, setFilterBy] = useState('');
+
+    const handleFilterBy = (filterBy) => {
+        setFilterBy(filterBy);
+    }
+
+    const filteredAssignments = filterBy ? allAssignment.filter(assignment => {
+        return assignment.difficultyLevel === filterBy;
+    }) : allAssignment;
+
     return (
         <div>
+            <div className="text-center items-center justify-center">
+                <details className="dropdown">
+                    <summary className="btn bg-pink-400 mt-4 text-black px-6 py-4 m-1 hover:bg-white hover:text-black border-2 border-pink-500 hover:border-pink-300">Filter</summary>
+                    <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+                        <li><button onClick={() => handleFilterBy('Easy')}>Easy</button></li>
+                        <li><button onClick={() => handleFilterBy('Medium')}>Medium</button></li>
+                        <li><button onClick={() => handleFilterBy('Hard')}>Hard</button></li>
+                    </ul>
+                </details>
+            </div>
+
             <div className="grid md:grid-cols-2 lg:grid-cols-2 lg:gap-5 md:gap-2">
-                {allAssignment.map(assignments => <AllAssignments key={assignments._id} assignments={assignments}></AllAssignments>)}
+                {filteredAssignments.length > 0 ? (
+                    filteredAssignments.map(assignments => <AllAssignments key={assignments._id} assignments={assignments}></AllAssignments>)
+                ) : (
+                    allAssignment.map(assignments => <AllAssignments key={assignments._id} assignments={assignments}></AllAssignments>)
+                )}
             </div>
         </div>
     );
--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -1,4 +1,4 @@
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 import AllAssignments from "../AllAssignments/AllAssignments";
 
 const Features = () => {
@@ -17,6 +17,12 @@ const Features = () => {
 
             <div className="grid md:grid-cols-2 lg:grid-cols-2 lg:gap-5 md:gap-2">
                 {limitedAssignments.map(assignments => <AllAssignments key={assignments._id} assignments={assignments}></AllAssignments>)}
+            </div>
+
+            <div>
+                <div className="mt-6 flex justify-center items-center">
+                   <Link to='/assignments'><button className="btn bg-pink-400 border-pink-800 text-black text-xl hover:bg-white hover:text-black hover:border-pink-700">See More</button></Link> 
+                </div>
             </div>
 
         </div>
