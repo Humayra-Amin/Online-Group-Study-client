c88613592a69c90e1e3bf88574192eaa47f68883
Humayra-Amin <humaema4576@gmail.com>
2024-05-17 19:38:04 +0600

    my assignment modified and added more functions

--- src/components/MyAssignment/MyAssignment.jsx
+++ src/components/MyAssignment/MyAssignment.jsx
@@ -1,34 +1,34 @@
-// import { Helmet } from "react-helmet-async";
-// import { useLoaderData } from "react-router-dom";
-// import MyAssignmentCard from "../MyAssignmentCard/MyAssignmentCard";
+import { Helmet } from "react-helmet-async";
+import { useLoaderData } from "react-router-dom";
+import MyAssignmentCard from "../MyAssignmentCard/MyAssignmentCard";
 
-// const MyAssignment = () => {
-//     const assignment = useLoaderData();
+const MyAssignment = () => {
+    const assignment = useLoaderData();
 
-//     return (
-//         <div className='flex flex-col items-center'>
+    return (
+        <div className='flex flex-col items-center'>
 
-//             <Helmet>
-//                 <title>Study Bizz | My Assignment</title>
-//                 <link rel="icon" type="image/jpg" href="/src/assets/images/icon.png" />
-//             </Helmet>
+            <Helmet>
+                <title>Study Bizz | My Assignment</title>
+                <link rel="icon" type="image/jpg" href="/src/assets/images/icon.png" />
+            </Helmet>
 
-//             <h1 className="mt-10 text-5xl font-sedan text-center">My Assignment</h1>
+            <h1 className="mt-10 text-5xl font-sedan text-center">My Assignment</h1>
 
-//             <div className="overflow-x-auto mt-14">
+            <div className="overflow-x-auto mt-14">
 
 
-//                 <div className="grid md:grid-cols-2 lg:grid-cols-2 gap-5 lg:gap-5 md:gap-2">
-//                     {
-//                         assignment.map(assignments => <MyAssignmentCard key={assignments._id} assignments={assignments} allAssignment={assignment}></MyAssignmentCard >)
-//                     }
+                <div className="grid md:grid-cols-2 lg:grid-cols-2 gap-5 lg:gap-5 md:gap-2">
+                    {
+                        assignment.map(assignments => <MyAssignmentCard key={assignments._id} assignments={assignments} allAssignment={assignment}></MyAssignmentCard >)
+                    }
 
-//                 </div>
-//             </div>
+                </div>
+            </div>
 
-//         </div>
+        </div>
 
-//     );
-// };
+    );
+};
 
-// export default MyAssignment;
+export default MyAssignment;
--- src/components/MyAssignmentCard/MyAssignmentCard.jsx
+++ src/components/MyAssignmentCard/MyAssignmentCard.jsx
@@ -1,24 +1,24 @@
 
-// const MyAssignmentCard = ({ assignments }) => {
+const MyAssignmentCard = ({ assignments }) => {
 
-//     const { title, status, marks, obtainedMarks, feedback } = assignments;
+    const { title, status, marks, obtainedMarks, feedback } = assignments;
 
 
-//     return (
+    return (
 
-//         <div>
-//             <div className="card w-96 bg-base-100 shadow-xl border-2 border-pink-600">
-//                 <div className="card-body lg:h-[280px]">
-//                     <h2 className="card-title lg:text-2xl font-sedan">Assignment Title: {title}</h2>
-//                     <p className="font-bold text-xl">Status: <span className="font-normal">{status}</span></p>
-//                     <p className="font-bold text-xl">Marks: <span className="font-normal">{marks}</span></p>
-//                     <p className="font-bold text-xl">Obtained Marks: <span className="font-normal">{obtainedMarks}</span></p>
-//                     <p className="font-bold text-xl">FeedBack: <span className="font-normal">{feedback}</span></p>
-//                 </div>
-//             </div>
+        <div>
+            <div className="card w-96 bg-base-100 shadow-xl border-2 border-pink-600">
+                <div className="card-body lg:h-[280px]">
+                    <h2 className="card-title lg:text-2xl font-sedan">Assignment Title: {title}</h2>
+                    <p className="font-bold text-xl">Status: <span className="font-normal">{status}</span></p>
+                    <p className="font-bold text-xl">Marks: <span className="font-normal">{marks}</span></p>
+                    <p className="font-bold text-xl">Obtained Marks: <span className="font-normal">{obtainedMarks}</span></p>
+                    <p className="font-bold text-xl">FeedBack: <span className="font-normal">{feedback}</span></p>
+                </div>
+            </div>
 
-//         </div>
-//     );
-// };
+        </div>
+    );
+};
 
-// export default MyAssignmentCard;
+export default MyAssignmentCard;
